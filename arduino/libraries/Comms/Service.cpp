fbb794635161b441fcf77412bd63ec9e74010d63
R. David Dunphy <rdd.dunphy@gmail.com>
2016-11-15 02:40:23 +0000

    Start writing Service class.

--- arduino/libraries/Comms/Service.cpp
+++ arduino/libraries/Comms/Service.cpp
@@ -1,13 +1,40 @@
 #include "Service.h"
+#include <iterator>
 
-Service::Service() {
-	// TODO - implement Service::Service
+Service::Service(byte id, String name) {
+	_id = id;
+	_name = name;
 }
 
+/* Returns the ID number of the service. */
+byte Service::getId() {
+	return _id;
+}
+
+/* Returns the name of the service. */
+String Service::getName() {
+	return _name;
+}
+
+/* Returns the port with the shortest associated distance to this service. */
 Port Service::getOutgoingPort() {
-	// TODO - implement Service::getOutgoingPort
+	Port p;
+	int d = -1;
+	std::map<Port, int>::iterator it = _portMap.begin();
+	while(it != _portMap.end()) {
+		if (d < 0 || it->second < d) {
+			d = it->second;
+			p = it->first;
+		}
+	}
+	return p;
 }
 
+/* Sets the associated distance for this service on a particular port. Will only 
+overwrite an existing entry if the new distance is less than the old distance. */
 void Service::setPortDistance(Port port, int distance) {
-	// TODO - implement Service::setPortDistance
+	if (_portMap.find(port) == _portMap.end()
+		|| _portMap[port] > distance) {
+		_portMap[port] = distance;
+	}
 }
--- arduino/libraries/Comms/Service.h
+++ arduino/libraries/Comms/Service.h
@@ -2,15 +2,20 @@
 #define Service_h
 
 #include "Port.h"
+#include <map>
 
 class Service {
 
+private:
+	byte _id;
+	String _name;
+	std::map<Port, int> _portMap;
 
 public:
-	Service();
-
+	Service(byte id, String name);
+	byte getId();
+	String getName();
 	Port getOutgoingPort();
-
 	void setPortDistance(Port port, int distance);
 };
 
