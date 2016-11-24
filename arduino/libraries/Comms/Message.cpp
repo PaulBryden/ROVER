dbd1fb8d4d21df96d3e38bcacf082a0c49d91372
Kyle Miller <yqb14153@uni.strath.ac.uk>
2016-11-24 18:56:15 +0000

    Added another test

--- arduino/libraries/Comms/Message.cpp
+++ arduino/libraries/Comms/Message.cpp
@@ -18,8 +18,8 @@ Message::Message(byte messageID, byte targetService, byte sourceService, byte ty
 	
 	//Integer division always rounds down so adding one to give desired result
 	_messageHeader.numOfPacketsInMessage = ((MESSAGE_HEADER_SIZE + _bodyContent.size())/PACKET_CONTENT_SIZE) + 1;
-	_messageHeader.typeOfMessage = _typeOfMessage;
-	_messageHeader.messageBitfields = _messageBitfields;
+	_messageHeader.typeOfMessage = typeOfMessage;
+	_messageHeader.messageBitfields = messageBitfields;
 }
 
 vector<packet_t> Message::toPackets() {
@@ -50,22 +50,25 @@ vector<packet_t> Message::toPackets() {
 //if resource discover 2/7 pass on if not serial.println
 void Message::readMessage() {
 
-#define MT_PASS_ON 00000111
-#define MT_ADVERTISE 00000010
-#define MT_STOP 00001000
+#define MT_PASS_ON 0x07
+#define MT_ADVERTISE 0x02
+#define MT_STOP 0x08
 
 	// TODO - implement Message::readMessage
-	if (_messageHeader.typeOfMessage == MT_PASS_ON) {
+	if (_messageHeader.typeOfMessage == 0x07) {
 		//initate ResourceDiscover and pass on
+		printf("This is an RD message to pass on.\n");
 	}
 	else if (_messageHeader.typeOfMessage == MT_ADVERTISE) {
 		//Advertisement
+		printf("This is an advertise message.\n");
 	}
 	else if (_messageHeader.typeOfMessage == MT_STOP) {
 		//Inititate Resource Discovery and Stop
+		printf("This is a RD message, not to pass on!\n");
 	}
 	else {
 		//DEBUG
-		printf("This is not a resource discovery message.");
+		printf("This is not a resource discovery message.\n");
 	}
 }
--- arduino/libraries/Test/MessageTest.cpp
+++ arduino/libraries/Test/MessageTest.cpp
@@ -2,6 +2,23 @@
 #include "../Comms/MessageQueue.cpp"
 #include <stdio.h>
 
+void messageQueueTest(){
+		vector<byte> testContent;
+	testContent.push_back(0x56);
+	testContent.push_back(0x56);		
+	testContent.push_back(0x56);
+	testContent.push_back(0x56);
+	testContent.push_back(0x56);		
+	testContent.push_back(0x56);
+	testContent.push_back(0x56);
+	testContent.push_back(0x56);		
+	testContent.push_back(0x56);
+	testContent.push_back(0x56);
+	testContent.push_back(0x56);		
+	Message m1((byte) 0x00,(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, testContent);
+}
+
+
 int makeMessage(){
 	vector<byte> testContent;
 	testContent.push_back(0x56);
@@ -22,11 +39,22 @@ int makeMessage(){
 	testContent.push_back(0x56);
 	testContent.push_back(0x56);		
 	testContent.push_back(0x56);
-	Message m1((byte) 0x00,(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, testContent);
+	
+	//The readMessage must be called immediately after creation or  doesn't work
+	// "Do not know why" - Kyle
+	Message m1((byte) 0x00,(byte) 0x00, (byte) 0x00, (byte) 0x02, (byte) 0x00, testContent);
+	m1.readMessage();
+	Message m2((byte) 0x00,(byte) 0x00, (byte) 0x00, (byte) 0x07, (byte) 0x00, testContent);
+	m2.readMessage();
+	Message m3((byte) 0x00,(byte) 0x00, (byte) 0x00,  0x08, (byte) 0x00, testContent);
+	m3.readMessage();
+	Message m4((byte) 0x00,(byte) 0x00, (byte) 0x00,  0x00, (byte) 0x00, testContent);
+	m4.readMessage();
 	return 1;
 }
 
 int main(){
+	printf("test One:\n");
 	printf("Make Message : %d", makeMessage());
 	return 1;
 	
