95906fd88d6431dfc776ecf5367fe9ccb18a2e87
R. David Dunphy <raymond.dunphy.2014@uni.strath.ac.uk>
2016-11-24 03:28:14 +0000

    Write port code.

--- arduino/libraries/Comms/Port.cpp
+++ arduino/libraries/Comms/Port.cpp
@@ -1,17 +1,68 @@
 #include "Port.h"
 #include <HardwareSerial.h>
 
+#define STARTBYTE 0xfe
+#define ENDBYTE 0xff
+
 Port::Port(HardwareSerial *serial) {
 	_serial = serial;
+	_start_last = false;
+	_end_last = false;
+	_packet_start_rcvd = false;
 }
 
 void Port::read() {
-	// Read from _serial and add to _buffer
-	// TODO - implement Port::read
+	while (_serial->available > 0) {
+		byte b = _serial->read();
+		if (b == STARTBYTE) {
+			if (_start_last) { // byte-stuffed start byte
+				if (_packet_start_rcvd) {
+					_buffer.push_back(b);
+					_start_last = false;
+				}
+			} else if (_end_last) {
+				// packet end
+				packet_t p = getPacketFromBuffer();
+				// send the packet to the packet queue
+				// clear the buffer
+				_packet_start_rcvd = false;
+			} else {
+				_start_last = true;
+			}
+			_end_last = false;
+		} else if (b == ENDBYTE) {
+			if (_end_last) { // byte-stuffed end byte
+				if (_packet_start_rcvd) {
+					_buffer.push_back(b);
+					_end_last = false;
+				}
+			} else {
+				_end_last = true;
+			}
+			_start_last = false;
+		} else {
+			if (_start_last) {
+				// start of new packet
+				// clear the buffer
+				_start_last = false;
+				_packet_start_rcvd = true;
+			} else if (_end_last) {
+				// something went wrong - we have a single end byte
+				// clear the buffer
+				_packet_start_rcvd = false;
+			}
+			if (_packet_start_rcvd) {
+				_buffer.push_back(b);
+			}
+		}
+	}
 }
 
-void Port::write(vector<char> packet) {
-	_serial->write("hi");
+void Port::write(vector<byte> packet) {
+    std::vector<byte>::const_iterator it;
+	for (it = packet.begin(); it != packet.end(); ++it) {
+    	_serial->write(*it);
+	}
 }
 
 packet_t Port::getPacketFromBuffer() {
