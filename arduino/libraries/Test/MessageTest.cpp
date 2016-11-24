c1da4b4aa9b80ff9a29855028490bc66a3e3423c
Kyle Miller <yqb14153@uni.strath.ac.uk>
2016-11-24 19:16:27 +0000

    Changed test method

--- arduino/libraries/Test/MessageTest.cpp
+++ arduino/libraries/Test/MessageTest.cpp
@@ -41,15 +41,16 @@ int makeMessage(){
 	testContent.push_back(0x56);
 	
 	//The readMessage must be called immediately after creation or  doesn't work
-	// "Do not know why" - Kyle
-	Message m1((byte) 0x00,(byte) 0x00, (byte) 0x00, (byte) 0x02, (byte) 0x00, testContent);
+	// "Don't know why"
+	static Message m1((byte) 0x00,(byte) 0x00, (byte) 0x00, (byte) 0x02, (byte) 0x00, testContent);
 	m1.readMessage();
-	Message m2((byte) 0x00,(byte) 0x00, (byte) 0x00, (byte) 0x07, (byte) 0x00, testContent);
+	static Message m2((byte) 0x00,(byte) 0x00, (byte) 0x00, (byte) 0x07, (byte) 0x00, testContent);
 	m2.readMessage();
-	Message m3((byte) 0x00,(byte) 0x00, (byte) 0x00,  0x08, (byte) 0x00, testContent);
+	Message m3((byte) 0x00,(byte) 0x00, (byte) 0x00, (byte) 0x08, (byte) 0x00, testContent);
 	m3.readMessage();
-	Message m4((byte) 0x00,(byte) 0x00, (byte) 0x00,  0x00, (byte) 0x00, testContent);
+	Message m4((byte) 0x00,(byte) 0x00, (byte) 0x00,  (byte) 0x00, (byte) 0x00, testContent);
 	m4.readMessage();
+	m2.readMessage();
 	return 1;
 }
 
