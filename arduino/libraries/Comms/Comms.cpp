b73fd39be39f67b04a5a8d2901ffc689256e0bf3
Peter De Jonckheere <dkb14157@ds.strath.ac.uk>
2016-11-25 11:48:44 +0000

    Fixed the merge

--- arduino/libraries/Comms/Comms.cpp
+++ arduino/libraries/Comms/Comms.cpp
@@ -5,34 +5,34 @@ void Comms::sendMessage(Message m) {
 	vector<packet_t> packets;
 	packets = m.toPackets();
 	
-	byte target = message._targetService;
+	byte target = m._targetService;
 	
-	Service s = serviceTable.getService(target);
+	Service * s = serviceTable.getService(target);
 	
-	if (Service.getShorttestDistance() > 0 ) {
-		Port p = Service.getOutgoingPort();
+	if (s -> getShortestDistance() > 0 ) {
+		Port p = s -> getOutgoingPort();
 		
 		for (int i; packets.size() > i; i++) {
 			handle.sendPacket(packets[i], p);
 		}
 	} else {
-		message.readMessage();
+		m.readMessage();
 	}
 	
 }
 
 void Comms::checkMessages() {
 	// TODO - implement Comms::checkMessages
-	message = messageQueue.popMessage();
+	Message message = messageQueue.popMessage();
 	message.readMessage();
 }
 
-void Comms::initialiseNode(HardwareSerial* serials[]) {
+void Comms::initialiseNode(Port* serials[]) {
 	// TODO create static variables
 	//vector<Port> portList(ports, ports + sizeof ports / sizeof ports[0]);
-	vector<Port> portList;
-	for (int i = 0; i < (sizeof(serials) / sizeof(serials[0])); i++) {
-		portList[i] = Port(serials[i]);
+	//vector<Port> portList;
+	for (int i = 0; i < (sizeof(*serials) / sizeof(serials[0])); i++) {
+		portList[i] = *serials[i];
 	}
 	
 	// TODO Do resource discovery stuff
