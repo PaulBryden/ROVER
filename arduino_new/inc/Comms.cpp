c0d86187087c4ec9029b3743aec9d259f26070ae
R. David Dunphy <raymond.dunphy.2014@uni.strath.ac.uk>
2017-01-20 12:35:45 +0000

    Tidied up directory structure and removed binary files.

--- arduino_new/inc/Comms.cpp
+++
@@ -1,8 +0,0 @@
-#include <Comms.h>
-
-MessageQueue Comms::messageQueue= MessageQueue();
-ServiceTable Comms::serviceTable=ServiceTable();
-std::vector<Port*> Comms::portList=vector<Port*>();
-PacketQueue Comms::packetQueue=PacketQueue();
-Comms::Comms() {
-}
--- arduino_old/libraries/Test/HandleTestTest.cpp
+++
@@ -1,7 +0,0 @@
-#ifndef Arduino_h
-#include <stdio.h>
-#endif
-
-int main(){
-  printf("KILL ME");
-}
